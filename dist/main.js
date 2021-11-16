4c1d1629eb99b40f763faf531a9fae6fa3e25d32
Nick P <nickjpossley@gmail.com>
2021-11-15 21:31:52 -0800

    fix append again

--- dist/main.js
+++ dist/main.js
@@ -47,7 +47,7 @@
             script.onload = () => resolve(script);
             script.onerror = () => reject(new Error(`Script load error for ${src}`));
         
-            document.appendChild(script);
+            document.body.appendChild(script);
           });
     }
 
@@ -58,11 +58,11 @@
             var css_link = $("<link>", { 
                 rel: "stylesheet", 
                 type: "text/css", 
-                href: "https://cdn.jsdelivr.net/gh/np-ally/tadpoll@0.0.3/dist/main_style.css" 
+                href: "https://cdn.jsdelivr.net/gh/np-ally/tadpoll@0.0.4/dist/main_style.css" 
             });
             css_link.appendTo('head');  
             
-            var id = getParams("np-ally/tadpoll@0.0.3/dist/main.js");
+            var id = getParams("np-ally/tadpoll@0.0.4/dist/main.js");
             
             getUserParams(id)
             .then(custParams => {
@@ -82,10 +82,10 @@
                     //console.log(video_query);
 
                     /*****Load video and form scripts****/
-                    loadScript("https://cdn.jsdelivr.net/gh/np-ally/tadpoll@0.0.3/dist/youtube_demo.js" + video_query, "text/javascript")
+                    loadScript("https://cdn.jsdelivr.net/gh/np-ally/tadpoll@0.0.4/dist/youtube_demo.js" + video_query, "text/javascript")
                     .then(script => {
                         if (custParams.fields.form === "default"){
-                            loadScript("https://cdn.jsdelivr.net/gh/np-ally/tadpoll@0.0.3/dist/form.js", "text/javascript");
+                            loadScript("https://cdn.jsdelivr.net/gh/np-ally/tadpoll@0.0.4/dist/form.js", "text/javascript");
                         }
                         //can add other form options here depending on the customer config
                         //else if (custParams.fields.form === TBD){}
